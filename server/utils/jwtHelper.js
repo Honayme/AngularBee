442f8d073bfab0b881f457ce4e34759d87f609dc
Honayme <honayme@gmail.com>
2018-05-17 10:58:32 +0200

    Add utils folder and insert method valiation for jwt inside

---
+++ server/utils/jwtHelper.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken'),
+  SECRET = 'secret'; //TODO https://www.youtube.com/watch?v=F0HLIe3kNvM
+
+
+const generateUserToken = (userData) => {
+  return jwt.sign({
+      userId: userData.id
+    },
+    SECRET,
+    {
+      expiresIn: '1h'
+    })
+};
+
+const parseAuthorization = (authorization) => {
+  return (authorization != null) ? authorization.replace('Bearer ', '') : null;
+};
+
+const getUserId = (authorization) => {
+  let userId = -1;
+  let token = module.exports.parseAuthorization(authorization); // Module.exports pour préciser qu'on est dans le même module
+  if(token != null){
+    try{
+      let jwtToken = jwt.verify(token, SECRET);
+      if(jwtToken != null)
+        userId = jwtToken.userId;
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  return userId
+};
+
+module.exports = {
+  generateUserToken,
+  parseAuthorization,
+  getUserId
+};
+
+//TODO Rename utils and not Helper cuz this is something different
