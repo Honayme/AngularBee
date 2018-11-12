32c858d7c3750a9ce332afccecdc8bb724684975
Honayme <honayme@gmail.com>
2018-11-12 12:41:19 +0100

    [Angular] Handle the login form connexion and error

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { AppComponent } from '../../app.component';
+import { Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../auth.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {IUser} from '../iuser';
 
 declare var $: any ;
 
@@ -10,16 +13,45 @@ declare var $: any ;
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = <IUser>{};
+  loginForm: FormGroup;
+  loginInvalid = false;
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private fb: FormBuilder) {}
 
   close(){
     $('#login').modal('close');
   }
 
+  postLogin() {
+    this.loginData = Object.assign(this.loginData, this.loginForm.value);
+    this.authService.logInUser(this.loginData).subscribe(res => {
+      if (!res) {
+        this.loginInvalid = true;
+        console.log(this.loginData);
+      } else {
+        this.close();
+        this.router.navigate(['/home']);
+      }
+    });
+  }
+
   ngOnInit() {
+    // Form //
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    // jQuery //
     $(document).ready(function(){
       $('#login').modal();
     });
+
+    //TODO Appuyer sur entrée pour valider le formulaire de login
   }
+
 
 }
